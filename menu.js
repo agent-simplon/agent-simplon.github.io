ecae545fb4bb8c571055e4eea77b48e4001ee0f5
Abdel-Malik <Abdelmalik.bouali@yahoo.com>
2015-12-03 10:47:50 +0100

    modification du menu.html, menu.css, menu.js et style.css

--- menu.js
+++ menu.js
@@ -1,9 +1,19 @@
 $(function(){
     $('.profil').click(function(){
-        $('.profil2').css('visibility', 'visible');
-        $('.profil3').css('visibility', 'visible');
-        $('.profil3-bis').css('visibility', 'visible');
-        $('.profil4').css('visibility', 'visible');
-        $('.profil5').css('visibility', 'visible');
+        $('.profil2').css('visibility', 'visible').click(function(){
+          $('.presentation').html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque arcu diam, ultricies sit amet dolor porttitor, venenatis </p>")
+        });
+        $('.profil3').css('visibility', 'visible').click(function(){
+          $('.presentation').html("<p>test2</p>")
+        });
+        $('.profil3-bis').css('visibility', 'visible').click(function(){
+          $('.presentation').html("<p>test3</p>")
+        });
+        $('.profil4').css('visibility', 'visible').click(function(){
+          $('.presentation').html("<p>test4</p>")
+        });
+        $('.profil5').css('visibility', 'visible').click(function(){
+          $('.presentation').html("<p>test5</p>")
+        });
         });
     });
