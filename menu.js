02597b0d0996ecd7c0a355b8d22cf7e7d2c984ef
jerome <fanniojerome@free.fr>
2015-12-05 21:55:26 +0100

    prise correctif adel modif

--- menu.js
+++ menu.js
@@ -1,21 +1,15 @@
 $(function() {
-  $('.profil').click(function() {
-    $('.profil2').css('visibility', 'visible').click(function() {
-
-      $('.presentation').html("<h1>NEDJADI Ahmed</h1><p>Salut Ô peuple des internautes au nez propre bienvenue dans notre insolente organisation nos qualiter et notre reputaion n'etant plus a faire (on a le droit de rever)je me permet de me presenter pour vous faire rever de mon experience extraordinaire sur les crotte de nez n hesiter surtout pas a me questionner concernant ce probleme du tout a chacun je saurais vous diriger pour ne pas avoir a vous repondre ou si vous avez besoin de quoi que ce soit pour vous dire comment vous en passez A biento sur nos ligne</p> <a href=\"http://agent-simplon.github.io/cvAhmed.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
-
-    };
-    $('.profil3').css('visibility', 'visible').click(function() {
-      $('.presentation').html("<h1>BOUALI Abdel-Malik</h1><p>Le premier souvenir dont je me souvien de cet époque est la bouche de metro de brodway, a new york, en 2004.Comment suis je arriver là,l entreprise flash gordon à crée ,pardon vas crée en 2148 un generateur de graviter ou j etait convier suite a un souci technique j'ai voyager dans le temps,car oui... Je vien du futur</p> <a href=\"http://agent-simplon.github.io/cvbouali.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
-    });
-    $('.profil3-bis').css('visibility', 'visible').click(function(){
-      $('.presentation').html("<h1>FANNIO Jérome</h1><p>L'informatique est au coeur de ma vie. Elle me passionne dans son étude et sa pratique. Créer des sites internet, créer des applications me passionne ennormément.</p> <a href=\"http://agent-simplon.github.io/cv2.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
-    });
-    $('.profil4').css('visibility', 'visible').click(function() {
-      $('.presentation').html("<h1>M'CHINDRA Adel-Ben</h1><p>Jeune aventurier des temps modernes, il parcours le monde virtuel à la recherche du saint costard .</p> <a href=\"http://agent-simplon.github.io/cvslyz.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
-    });
-    $('.profil5').css('visibility', 'visible').click(function() {
-      $('.presentation').html("<h1>SERY Nadege</h1><p>Un vent d’optimisme souffle sur le numérique français, certains parlent même de troisième révolution industrielle à la française, et l’ensemble du pays ne peut que s’en réjouir. Progressivement, le numérique s’est imposé à tous. Il n’est plus réservé aux technophiles, il n’est plus non plus considéré comme une niche par les acteurs économiques, mais comme une vague de fond qui touche tous les secteurs de façon transversal.</p><a href=\"http://agent-simplon.github.io/cvnadege.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
-    });
-  });
-});
+      $('.profil').click(function() {
+          $('.profil2').css('visibility', 'visible').click(function() {
+              $('.presentation').html("<h1>NEDJADI Ahmed</h1><p>Salut Ô peuple des internautes au nez propre bienvenue dans notre insolente organisation nos qualiter et notre reputaion n'etant plus a faire (on a le droit de rever)je me permet de me presenter pour vous faire rever de mon experience extraordinaire sur les crotte de nez n hesiter surtout pas a me questionner concernant ce probleme du tout a chacun je saurais vous diriger pour ne pas avoir a vous repondre ou si vous avez besoin de quoi que ce soit pour vous dire comment vous en passez A biento sur nos ligne</p> <a href=\"http://agent-simplon.github.io/cvAhmed.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
+            }; $('.profil3').css('visibility', 'visible').click(function() {
+              $('.presentation').html("<h1>BOUALI Abdel-Malik</h1><p>Le premier souvenir dont je me souvien de cet époque est la bouche de metro de brodway, a new york, en 2004.Comment suis je arriver là,l entreprise flash gordon à crée ,pardon vas crée en 2148 un generateur de graviter ou j etait convier suite a un souci technique j'ai voyager dans le temps,car oui... Je vien du futur</p> <a href=\"http://agent-simplon.github.io/cvbouali.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
+            }); $('.profil3-bis').css('visibility', 'visible').click(function() {
+              $('.presentation').html("<h1>FANNIO Jérome</h1><p>L'informatique est au coeur de ma vie. Elle me passionne dans son étude et sa pratique. Créer des sites internet, créer des applications me passionne ennormément.</p> <a href=\"http://agent-simplon.github.io/cv2.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
+            }); $('.profil4').css('visibility', 'visible').click(function() {
+              $('.presentation').html("<h1>M'CHINDRA Adel-Ben</h1><p>Jeune aventurier des temps modernes, il parcours le monde virtuel à la recherche du saint costard .</p> <a href=\"http://agent-simplon.github.io/cvslyz.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
+            }); $('.profil5').css('visibility', 'visible').click(function() {
+              $('.presentation').html("<h1>SERY Nadege</h1><p>Un vent d’optimisme souffle sur le numérique français, certains parlent même de troisième révolution industrielle à la française, et l’ensemble du pays ne peut que s’en réjouir. Progressivement, le numérique s’est imposé à tous. Il n’est plus réservé aux technophiles, il n’est plus non plus considéré comme une niche par les acteurs économiques, mais comme une vague de fond qui touche tous les secteurs de façon transversal.</p><a href=\"http://agent-simplon.github.io/cvnadege.html\"<div class=\"btn-cv\">Voir le CV</div></a>")
+            });
+          });
+      });
